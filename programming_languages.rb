1dbbe92f6764b187c680fa6ce3e825d5b323b80a
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-16 00:09:54 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,9 @@
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  languages.each do |styles, languagez|
+    languagez.each do |lang, type|
+      type_value_arr = type.values
+      type_value = type_value_arr[0]
+      new_hash[lang] = {type: type_value, }
+
 end
