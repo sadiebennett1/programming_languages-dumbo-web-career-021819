f91475d5d2b7eb7f747554e3196331056a4835a4
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-16 00:19:06 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,19 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |styles, languagez|
+    languagez.each do |lang, type|
+      if new_hash.keys.include?(lang)
+        new_hash[lang][:style] << styles
+      else
+        type_value_arr = type.values
+        type_value = type_value_arr[0]
+        new_hash[lang] = {:type => type_value, :style => [styles]}
+      end
+    end
+  end
+  return new_hash
+      # type_value_arr = type.valuesx
+      # type_value = type_value_arr[0]
+      # new_hash[lang] = {type: type_value, }
 end
